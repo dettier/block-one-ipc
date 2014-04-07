6bc5faa11132bc8f6d4998b44e397578de3b4cd0
Eugene <Dolgy>
2014-04-07 20:23:00 +0600

    0.1.4

--- git-bump.cmd
+++ git-bump.cmd
@@ -4,14 +4,14 @@ set version=%~1
 echo compiling..
 call gulp compile
 
-echo creating tag %version%..
-call git tag -a %version% -m "%version%"
-
 echo adding new files..
 call git add .
 
 echo commiting tag..
 call git commit -m "%version%"
+
+echo creating tag %version%..
+call git tag -a %version% -m "%version%"
 
 echo pushing..
 call git push
